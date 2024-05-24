9d17a88783fadda1189b7d202efb8fd9ab116128
Hajin Choi <j8747j@gmail.com>
2024-05-25 05:53:12 +0900

    값 검증

--- app/actions.ts
+++ app/actions.ts
@@ -2,17 +2,44 @@
 
 import { resolve } from "path";
 
-export async function handlePassword(preState:any, formData: FormData){
-    const inputPassword = formData.get("password")
-    console.log(" 입력한 비밀번호?")
-    console.log(inputPassword)
-    let error: string;
-    let welcome;
-        if(inputPassword == "12345"){
-            await new Promise((resolve) => setTimeout(resolve,3000))
-            return error = ""
-        }else if(inputPassword != "12345"){
-            return error = "Wrong password"
-        }
+import {string, z} from "zod"
+
+const formSchema = z.object({
+    username :  z.string().min(5, "Username should be at least 5 charactors long."),
+    email : z.string().email().refine((email) => email.includes("@zod.com") ? true : false, "Only @zod.com emails are allowed"),
+    password : z.string().min(10).refine((password)=> {
+        if(password.match(/[^0-9]/)){
+            return true}else{
+                return false
+            }}, "Password sould be contain at least number (0123456789)")
+})
+
+    export async function handlePassword(preState:any, formData: FormData){
+    const data = {
+        usename : formData.get("username"),
+        email : formData.get("email"),
+        password : formData.get("password")
+    }
+
+        const result = formSchema.safeParse(data)
+    if(!result.success){
+        console.log(data)
+        console.log(result.error.flatten)
+        return result.error.flatten()
+    }else{
+        console.log(result.data)
+    }
+
     
 }
+// const inputPassword = formData.get("password")
+// console.log(" 입력한 비밀번호?")
+// console.log(inputPassword)
+// let error: string;
+//     if(inputPassword == "12345"){
+//         await new Promise((resolve) => setTimeout(resolve,3000))
+//         return error = ""
+//     }else if(inputPassword != "12345"){
+//         return error = "Wrong password"
+
+//     }
--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ import Image from "next/image";
 import { useFormState } from "react-dom";
 import { handlePassword } from "./actions";
 import { error } from 'console';
+import FormInput from "@/components/form-input";
 
 
 export default function Home() {
 
   const [state, action] = useFormState(handlePassword, null)
-  console.log(" 스테이트 ")
-  console.log(state)
+
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-8 px-6 bg-red-300 gap-3">
@@ -21,9 +21,9 @@ export default function Home() {
       </div>
           <form action={action} className="flex flex-col gap-3 w-9/12">
             <div className="flex justify-center items-center relative">
-              <input className="rounded-2xl w-full h-10 ring-1 focus:-outline-offset-2 focus:outline-slate-300 focus:outline-1 ring-slate-300 border-none pl-10" name="email" type="text" placeholder="Email" required/>
+              <FormInput type="text" placeholder="이메일을 적어주세요" name="email" required errors={state?.fieldErrors.email}/>
                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute
-                          left-3 opacity-50 w-6 h-6">
+                          left-3 opacity-50 w-6 h-6 top-2">
               <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
               <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
             </svg>
@@ -31,29 +31,23 @@ export default function Home() {
 
 
             <div className="flex justify-center items-center relative">
-              <input className="rounded-2xl w-full h-10 ring-1 focus:-outline-offset-2 focus:outline-slate-300 focus:outline-1 ring-slate-300 border-none pl-10" 
-              type="text" placeholder="User" required name="usename"/>
-
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 absolute left-3 opacity-50">
+              <FormInput type="text" placeholder="UserName" name="username" required errors={state?.fieldErrors.username}/>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 absolute left-3 top-2 opacity-50">
                 <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
               </svg>
             </div>
 
-{/* 
             <div className="flex justify-center items-center relative">
-              <input className="rounded-2xl w-full h-10 ring-1 focus:-outline-offset-2 focus:outline-slate-300 focus:outline-1 ring-slate-300 border-none pl-10 invalid:ring-red-500 invalid:ring-2" 
-              type="text" placeholder="Password" required name="password"/>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 absolute left-3 opacity-50">
-            <path fillRule="evenodd" d="M15.75 1.5a6.75 6.75 0 0 0-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 0 0-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 0 0 .75-.75v-1.5h1.5A.75.75 0 0 0 9 19.5V18h1.5a.75.75 0 0 0 .53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1 0 15.75 1.5Zm0 3a.75.75 0 0 0 0 1.5A2.25 2.25 0 0 1 18 8.25a.75.75 0 0 0 1.5 0 3.75 3.75 0 0 0-3.75-3.75Z" clipRule="evenodd" />
-          </svg>
-            </div> */}
-            <PasswordInput error={state ?? ""}></PasswordInput>
-            {/* <span className="text-red-500 font-m" >Input Error</span> */}
-            {/* <button className="w-full h-12 bg-slate-200 rounded-3xl font-bold">Log in</button> */}
+                <FormInput type="password" placeholder="비밀번호 입력" name="password" required errors={state?.fieldErrors.password}/>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 absolute left-3 top-2 opacity-50">
+                <path fillRule="evenodd" d="M15.75 1.5a6.75 6.75 0 0 0-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 0 0-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 0 0 .75-.75v-1.5h1.5A.75.75 0 0 0 9 19.5V18h1.5a.75.75 0 0 0 .53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1 0 15.75 1.5Zm0 3a.75.75 0 0 0 0 1.5A2.25 2.25 0 0 1 18 8.25a.75.75 0 0 0 1.5 0 3.75 3.75 0 0 0-3.75-3.75Z" clipRule="evenodd" />
+                </svg>
+            </div>
+
             <LoginBtn text="Log in"/>
-            { state == "" ??
+            {/* { state?.formErrors.email  ??
                 <div className=" flex font-bold bg-green-500 h-14 rounded-2xl items-center text-2xl text-center justify-center"> Welcome back!</div>
-                }
+                } */}
           </form>
     </div>
   );
